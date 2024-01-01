a9e8b6e20e79a8ecdbbcecc081549bdeaa96a59b
niviten <l.j.nivethan@gmail.com>
2024-01-01 17:22:27 +0530

    go: Linked list

---
+++ go/ds/linkedlist/linkedlist.go
@@ -0,0 +1,117 @@
+package linkedlist
+
+import (
+    "fmt"
+)
+
+type node struct {
+    data int
+    next *node
+}
+
+type LinkedList struct {
+    head *node
+}
+
+func NewLinkedList() *LinkedList {
+    return &LinkedList{head: nil}
+}
+
+func (list *LinkedList) Print() {
+    fmt.Print("[")
+    walk := list.head
+    for walk != nil {
+        fmt.Printf("%d, ", walk.data)
+        walk = walk.next
+    }
+    fmt.Println("]")
+}
+
+func (list *LinkedList) Insert(data int) {
+    newNode := &node{data: data, next: nil}
+    if list.head == nil {
+        list.head = newNode
+        return
+    }
+    walk := list.head
+    for walk.next != nil {
+        walk = walk.next
+    }
+    walk.next = newNode
+}
+
+func (list *LinkedList) Delete(data int) {
+    var prewalk *node = nil
+    walk := list.head
+
+    for walk != nil && walk.data != data {
+        prewalk = walk
+        walk = walk.next
+    }
+
+    if walk == nil {
+        return
+    }
+
+    if prewalk == nil {
+        list.head = list.head.next
+        return
+    }
+
+    prewalk.next = walk.next
+}
+
+func (list *LinkedList) Search(searchKey int) int {
+    walk := list.head
+    index := 0
+    for walk != nil {
+        if walk.data == searchKey {
+            return index
+        }
+        index = index + 1
+        walk = walk.next
+    }
+    return -1
+}
+
+func (list *LinkedList) Length() int {
+    length := 0
+    walk := list.head
+    for (walk != nil) {
+        walk = walk.next
+        length = length + 1
+    }
+    return length
+}
+
+func (list *LinkedList) Reverse() *LinkedList {
+    walk := list.head
+    var n *node
+    n = nil
+    for walk != nil {
+        newNode := &node{data: walk.data, next: n}
+        n = newNode
+        walk = walk.next
+    }
+    newList := NewLinkedList()
+    newList.head = n
+    return newList
+}
+
+func (list *LinkedList) ReverseInplace() {
+    var curr *node
+    var prev *node
+    var temp *node
+
+    curr = list.head
+    prev = nil
+
+    for curr != nil {
+        temp = curr.next
+        curr.next = prev
+        prev = curr
+        curr = temp
+    }
+
+    list.head = prev
+}
