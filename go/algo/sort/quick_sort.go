794fabdbd5cfa650aa57fb8752124a6ca7b0f0b6
niviten <l.j.nivethan@gmail.com>
2024-01-29 20:52:38 +0530

    go: Refactor format

--- go/algo/sort/quick_sort.go
+++ go/algo/sort/quick_sort.go
@@ -1,38 +1,38 @@
 package sort
 
 func QuickSort(arr []int) {
-    quickSort(arr, 0, len(arr) - 1)
+	quickSort(arr, 0, len(arr)-1)
 }
 
 func quickSort(arr []int, low int, high int) {
-    if low >= high || low < 0 {
-        return
-    }
+	if low >= high || low < 0 {
+		return
+	}
 
-    p := partition(arr, low, high)
+	p := partition(arr, low, high)
 
-    quickSort(arr, low, p - 1)
-    quickSort(arr, p + 1, high)
+	quickSort(arr, low, p-1)
+	quickSort(arr, p+1, high)
 }
 
-func partition(arr[] int, low int, high int) int {
-    pivot := arr[high]
-    
-    i := low - 1
+func partition(arr []int, low int, high int) int {
+	pivot := arr[high]
 
-    for j := low; j <= high-1; j++ {
-        if arr[j] <= pivot {
-            i = i + 1
-            tmp := arr[i]
-            arr[i] = arr[j]
-            arr[j] = tmp
-        }
-    }
+	i := low - 1
 
-    i = i + 1
-    tmp := arr[i]
-    arr[i] = arr[high]
-    arr[high] = tmp
+	for j := low; j <= high-1; j++ {
+		if arr[j] <= pivot {
+			i = i + 1
+			tmp := arr[i]
+			arr[i] = arr[j]
+			arr[j] = tmp
+		}
+	}
 
-    return i
+	i = i + 1
+	tmp := arr[i]
+	arr[i] = arr[high]
+	arr[high] = tmp
+
+	return i
 }
